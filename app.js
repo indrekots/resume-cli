70b17006026e434574516bb2d85cbe5f8ae1bd3e
Indrek Ots <ots.indrek@gmail.com>
2016-08-13 12:47:00 +0300

    move arg parsing code to a separate module

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 var htmlWriter = require('./htmlWriter.js');
 var pdfWriter = require('./pdfWriter.js');
-var argv = require('yargs')
-  .usage('Usage: $0 [options]')
-  .example('$0 -i resume.md', 'Reads resume.md and generates an HTML and PDF doucment containing the resume content') 
-  .alias('i', 'input')
-  .demand('i', 'Specify path to the file which contains resume content in markdown')
-  .nargs('i', 1)
-  .describe('i', 'Resume content in markdown')
-  .alias('o', 'output')
-  .nargs('o', 1)
-  .describe('o', 'Output file name without file extension, if missing, output is written to "resume.html" and/or "resume.pdf"')
-  .argv;
+var argv = require('./argParser.js');
 
 var inputFile = argv.input;
 var outputFile = argv.output || 'resume';
+
 htmlWriter.write(inputFile, outputFile);
 pdfWriter.write(inputFile, outputFile);
---
+++ argParser.js
@@ -0,0 +1,13 @@
+var argv = require('yargs')
+  .usage('Usage: $0 [options]')
+  .example('$0 -i resume.md', 'Reads resume.md and generates an HTML and PDF doucment containing the resume content') 
+  .alias('i', 'input')
+  .demand('i', 'Specify path to the file which contains resume content in markdown')
+  .nargs('i', 1)
+  .describe('i', 'Resume content in markdown')
+  .alias('o', 'output')
+  .nargs('o', 1)
+  .describe('o', 'Output file name without file extension, if missing, output is written to "resume.html" and/or "resume.pdf"')
+  .argv;
+
+module.exports = argv;
