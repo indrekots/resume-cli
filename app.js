8ab1c7cbdfe2e90df618a8aaf54c74c790946531
Indrek Ots <ots.indrek@gmail.com>
2016-08-13 11:54:10 +0300

    add cli usage and exmaple command

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var htmlWriter = require('./htmlWriter.js');
 var pdfWriter = require('./pdfWriter.js');
 var argv = require('yargs')
+  .usage('Usage: $0 [options]')
+  .example('$0 -i resume.md', 'Reads resume.md and generates an HTML and PDF doucment containing the resume content') 
   .alias('i', 'input')
   .demand('i', 'Specify path to the file which contains resume content in markdown')
   .nargs('i', 1)
