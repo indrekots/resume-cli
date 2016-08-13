8b7ebde096c31038f958baa0434ba01b9f6c83ce
Indrek Ots <ots.indrek@gmail.com>
2016-08-13 11:48:45 +0300

    read argv for input file
    Use the yargs package to read argv and parse for '-i' or '--input' options which should point to the file which contains resume content in markdown format

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var htmlWriter = require("./htmlWriter.js");
-var pdfWriter = require("./pdfWriter.js");
+var htmlWriter = require('./htmlWriter.js');
+var pdfWriter = require('./pdfWriter.js');
+var argv = require('yargs')
+  .alias('i', 'input')
+  .demand('i', 'Specify path to the file which contains resume content in markdown')
+  .nargs('i', 1)
+  .describe('i', 'Resume content in markdown')
+  .argv;
 
-var inputFile = process.argv[2];
-htmlWriter.write(inputFile, "resume.html");
-pdfWriter.write(inputFile, "resume.pdf");
+var inputFile = argv.input;
+htmlWriter.write(inputFile, 'resume.html');
+pdfWriter.write(inputFile, 'resume.pdf');
